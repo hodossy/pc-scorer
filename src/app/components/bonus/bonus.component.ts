5d455412f34c338d4e6cedfabb0a1a08675e7ae7
szabolcs.hodossy <szabolcs.hodossy@commsignia.com>
2022-02-02 14:48:31 +0100

    feat: add missing icons + styles

--- src/app/components/bonus/bonus.component.ts
+++ src/app/components/bonus/bonus.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-bonus',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bonus.component.scss']
 })
 export class BonusComponent implements OnInit {
+  @Input() available: number = 0;
+  @Input() used: number = 0;
 
   constructor() { }
 
